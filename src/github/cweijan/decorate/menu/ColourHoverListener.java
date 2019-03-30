18ea3148bc0d7024709175351f79f1421a1f8ce2
cweijan <cweijan@163.com>
2019-03-30 17:41:51 +0800

    fix some bug

--- src/github/cweijan/decorate/menu/ColourHoverListener.java
+++ src/github/cweijan/decorate/menu/ColourHoverListener.java
@@ -1,8 +1,5 @@
 package github.cweijan.decorate.menu;
 
-import com.intellij.openapi.actionSystem.impl.ActionMenu;
-
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -18,36 +15,8 @@ public class ColourHoverListener extends MouseAdapter{
     }
 
     @Override
-    public void mouseEntered(MouseEvent e){
-
-        component.setBackground(Color.decode("#434C59"));
-        updateSubMenuUi(component);
-
-    }
-
-    @Override
     public void mouseExited(MouseEvent e){
         component.setBackground(defaultColor);
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e){
-        updateSubMenuUi(component);
-    }
-
-    private void updateSubMenuUi(Component component){
-
-        if(!(component instanceof ActionMenu)) return;
-        ActionMenu actionMenu = (ActionMenu) component;
-
-        MenuElement[] subElements = actionMenu.getPopupMenu().getSubElements();
-        for(MenuElement subElement : subElements){
-            if(subElement instanceof JMenuItem){
-                JMenuItem actionMenuItem = (JMenuItem) subElement;
-                if(actionMenuItem.getSubElements().length>0)continue;
-                actionMenuItem.setUI(new ColourMenuItemUi());
-            }
-        }
     }
 
 }
